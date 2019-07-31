02914779b0f089f0b8bcb709328205ce1dd33f43
Aleksey Morozov <moraleksey@gmail.com>
2019-07-31 23:14:06 +0300

    Замыкание

--- lesson10/script.js
+++ lesson10/script.js
@@ -47,3 +47,18 @@ let twelve = "12.2px";
 // console.log(Math.round(twelve));
 console.log(parseInt(twelve));
 console.log(parseFloat(twelve));
+
+//замыкание
+ function createCounter() {
+   let counter = 0;
+   const myFunction = function() {
+     counter = counter + 1;
+     return counter;
+   };
+   return myFunction;
+ }
+ const increment = createCounter();
+    const c1 = increment();
+    const c2 = increment();
+    const c3 = increment();
+    console.log('example increment', c1, c2, c3);
